d7a9de99299887264c6ba72e277023f40a2042f9
Ishikashail <ishikashail3002@gmail.com>
2024-07-01 18:24:34 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+$title = 'Online Ticket Reservation System';
+//E-Ticketing System For Railway
+$supervisor_name = "Mr.Ashish Kumar";
+$developer_name = "Ishika Shail";
+$developer_matric = "16/52HA016";
